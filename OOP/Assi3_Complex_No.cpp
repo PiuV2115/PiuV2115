f0cd520203b352cf0ece57f5829171826d052662
Priya Kshirsagar <144653180+PiuV2115@users.noreply.github.com>
2024-09-16 11:53:46 +0530

    Update Assi3_Complex_No.cpp

--- OOP/Assi3_Complex_No.cpp
+++ OOP/Assi3_Complex_No.cpp
@@ -4,6 +4,7 @@ class complex
 {   
     public:
     int real,img;
+    static int multreal,multimg;
     complex()
     {
         real=0;
@@ -16,19 +17,24 @@ class complex
     }
     void display()
     {
-        cout<<"Addition : "<<real<<" + "<<img<<"i";
-        cout<<"Multiplication : "<<x.real<<" + "<<x.img<<"i";
+        cout<<"\n----------Result------------\n";
+        cout<<"Addition : "<<real<<" + "<<img<<"i\n";
+    }
+    void multdisplay()
+    {
+       cout<<"Multiplication : "<<real<<" + "<<img<<"i\n";
     }
     void operator +(complex &x);
-    friend void operator *(complex &x,&y);
+    friend void operator *(complex &x,complex &y);
 };
    void complex::operator +(complex &x)
    {
        real=real+x.real;
        img=img+x.img;
    }
-   void operator *(complex &x, &y)
+   void operator *(complex &x, complex &y)
    {
+   
        x.real=(x.real*y.real)-(x.img*y.img);
        x.img=(x.real*y.img)+(y.real*x.real);
    }
@@ -39,9 +45,15 @@ int main()
     cin>>r1>>r2;
     cout<<"Enter Imaginary parts of two no. : ";
     cin>>i1>>i2;
+    cout<<"Complex Numbers Are : \n";
+    cout<<"1) "<<r1<<" + "<<i1<<"i\n";
+    cout<<"2) "<<r2<<" + "<<i2<<"i\n";
     complex obj1(r1,i1);
     complex obj2(r2,i2);
     obj1+obj2;
+    complex obj3(r1,i1);
+    obj3+obj2;
     obj1.display();
+    obj3.multdisplay();
     return 0;
 }
