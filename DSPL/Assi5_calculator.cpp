9f01a77b8c0c64d59d4086d20fb6fd33e4d041b7
Priya Kshirsagar <144653180+PiuV2115@users.noreply.github.com>
2024-09-26 11:51:04 +0530

    Update Assi5_calculator.cpp

--- DSPL/Assi5_calculator.cpp
+++ DSPL/Assi5_calculator.cpp
@@ -1,16 +1,13 @@
 #include<iostream>
-#include<cctype>
-#include<cstring>
-#include<unordered_set>
+#include<string>
 #define max 50
 using namespace std;
-class infix_to_postfix
-{
-  public:
-   char postfix[max];
-   char stack[max],temp;
-   int top=-1;
-   int isFull()
+ string postfix,infix;
+ class stackk
+ {
+  char stack[max];
+  int top=-1;
+  int isFull()
   {
     if(top>=max-1)
     	return 1;
@@ -32,7 +29,6 @@ class infix_to_postfix
    	cout<<"STACK IS EMPTY!!!\n";
    else
 {
-        temp=stack[top];
      	return stack[top--];
 }
 return 0;
@@ -50,100 +46,84 @@ return 0;
     stack[top]=x;
       }
   }
-  int instack(char y)
-  {
-   if(y=='+'||'-')
-    return 2;
-   else if(y=='*'||'/')
-    return 4;
-   else if(y=='^')
-    return 5;
-   else if(y=='(')
-    return 0;
-   else
-   {
-   
-   }
-   
+
+// friend bool isoperator(char op);
+ //friend int precedance(char y);
+ friend string infixtopostfix(string infix);
+ }obj;
  
-  }
-  int incoming(char z)
-  {
-  
-  if(z=='+'||'-')
-    return 1;
-   else if(z=='*'||'/')
-    return 3;
-   else if(z=='^')
-    return 6;
-   else if(z=='(')
-    return 9;
-   else
-    return 0;
-  
-  }
-   
- };
-
-
-int main()
-{
- infix_to_postfix obj;
- char infix[max],postfix[max];
- int count=0,flag=0;
- cout<<"Enter Infix Expression : ";
- cin.getline(infix,max);
- int len=strlen(infix);
- for(int i=0;i<len;i++)
+ int precedance(char op)
  {
-  /*for(char sam=a; sam<=z ; sam++ )
-  {
-    if(infix[i])
-    {
-    	postfix[count]=infix[i];
-    	count++;
-    
-    }
-  
-  }*/
-  flag=0;
- if(infix[i]=='*'||'+'||'/'||'^'||'-'||'('||')')
+ 	if(op=='+'||op=='-')
+ 	{
+ 		return 1;
+ 	}
+ 	else if(op=='*'||op=='/')
+ 	{
+ 		return 2;
+ 	}
+ 	else if(op=='^')
+ 	{
+ 		return 3;
+ 	}
+ 		return 0;
+ }
+ 
+ bool isoperator(char x)
  {
-  flag=1;
-  if(obj.isEmpty())
-  {
-   obj.push(infix[i]);
-  
-  }
-  else if(obj.instack(obj.top)>=obj.incoming(infix[i]))
-  {
-  obj.pop();
-  postfix[count]=obj.temp;
-  count++;
-  obj.push(obj.incoming(infix[i]));
-  }
-  else if(obj.instack(obj.top)<obj.incoming(infix[i]))
-  {
-   obj.push(obj.incoming(infix[i]));
-  }
-  else
-  {
-  }
+ 	return (x=='+'||x=='-'||x=='*'||x=='/');
  }
- if(flag==0)
+ 
+ string infixtopostfix(string infix)
  {
- postfix[count]=infix[i];
- count++;
- }
- }
- int temptop=obj.top;
- for(int i=temptop;i>-1;i--)
+   postfix="";
+   for(int i=0;i<infix.length();i++)
+   {
+   	char c=infix[i];
+   	if(isalnum(c))
+   	{
+   	
+ 	  	postfix+=c;
+ 
+   	}
+   	else if(c=='(')
+   	{
+   		obj.push(c);
+   	}
+   	else if(c==')')
+   	{
+   		while(!obj.isEmpty()&&obj.top=='(')
+   		{
+   			postfix+=obj.top;
+   			obj.pop();
+   		}
+   		obj.pop();
+   	}
+   	else if(isoperator(c))
+   	{
+   		while(!obj.isEmpty()&&precedance(obj.top)>=precedance(c))
+   		{
+   			postfix+=obj.top;
+   			obj.pop();	
+   		}
+   			obj.push(c);
+   	}
+   }
+ while(!obj.isEmpty())
  {
-obj.pop();
- postfix[count]=obj.temp;
-  count++;
+ 	postfix+=obj.top;
+ 	obj.pop();
  }
-//postfix[count]='/0';
-cout<<"Postfix Expression is "<<postfix;
-return 0;
-}
+ 
+ 		return postfix;
+ }
+ int main()
+ {
+ 	 cout<<"Enter Infix Expression :";
+ 	 cin>>infix;
+ 	 postfix=infixtopostfix(infix);
+ 	 cout<<"\nPostfix Expression is "<<postfix;
+ 	 cout<<"\n";
+ 	 return 0;
+ }
+ 
