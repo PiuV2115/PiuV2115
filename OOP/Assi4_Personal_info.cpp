013a522d5451288dc768ed554819b7c82e38bd18
Priya Kshirsagar <144653180+PiuV2115@users.noreply.github.com>
2024-11-06 21:28:45 +0530

    Update Assi4_Personal_info.cpp

--- OOP/Assi4_Personal_info.cpp
+++ OOP/Assi4_Personal_info.cpp
@@ -19,11 +19,11 @@ public:
  cin>>salary;
  }
 };
-class docter:public person
+class docter:virtual public person
 {
 public:
 	string speci,exper,room_no;
-	virtual void accept()
+    void accept()
 	{
 		cout<<"# Specialization :";
 		cin>>speci;
@@ -33,11 +33,11 @@ public:
 	    cin>>room_no;
 	}
 };
-class nurse:public person
+class nurse:virtual public person
 {
 public:
 	string shift,dept,ward;
-	virtual void accept()
+	void accept()
 	{
 		cout<<"# shift :";
 		cin>>shift;
@@ -50,9 +50,10 @@ public:
 class reception:public nurse,public docter
 {
 public:
- class reception obj;
     int choice;
     string tempname,tempid;
+    void fun(reception &obj)
+    {
 	cout<<"Enter your Choice :\n1.Docter \n2.Nurse";
 	cin>>choice;
 	switch(choice)
@@ -80,10 +81,13 @@ public:
 	    
 	    default:cout<<"Invalid Choice :";
 	}
+    }
 };
 int main()
 {
    int ch;
+   reception obj;
+   do{
    cout<<"1.Insert Docter Info \n2. Fill Nurse Info\n3.Reception\n4.Exit\nEnter your choice :";
    cin>>ch;
    switch(ch)
@@ -94,9 +98,31 @@ int main()
        cin>>size;
        for(int i=0;i<size;i++)
        {
-        obj.accept();   
+        obj.person::accept();
+        obj.docter::accept();
        }
+       break;
        
+        case 2:
+       int size1;
+       cout<<"Enter No of Nurse's info is to be Fill :";
+       cin>>size1;
+       for(int i=0;i<size1;i++)
+       {
+        obj.person::accept();
+        obj.nurse::accept();
+       }
+       break;
+       
+       case 3:
+       obj.fun(reception obj);
+       break;
+       
+       case 4: cout<<"You Are Exiting.....\n";
+       exit(0);
+       break;
+       
+       default:cout<<"Invalid Choice \n";
    }
-    
+   }while(1); 
 }
