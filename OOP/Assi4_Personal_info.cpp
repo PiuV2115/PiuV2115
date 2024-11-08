0ed671718850d4e8ae42a69f61e9ebc176e61d9b
Priya Kshirsagar <144653180+PiuV2115@users.noreply.github.com>
2024-11-08 06:37:26 +0530

    Update Assi4_Hospital_management.cpp

--- OOP/Assi4_Personal_info.cpp
+++ OOP/Assi4_Personal_info.cpp
@@ -1,47 +1,131 @@
-/tmp/TtZNK9c34y.o
-1.Insert Docter Info 
-2. Fill Nurse Info
-3.Reception
-4.Exit
-Enter your choice :1
-Enter No of Docter's info is to be Fill :2
-# Name :ankush
-# Age:30
-# Mobile No. :9699363381
-# ID :20
-# Salary :70000
-# Specialization :cardiologist
-# Experience. :2
-# Room No. :11
-# Name :piu
-# Age:20
-# Mobile No. :36784905
-# ID :30
-# Salary :20000
-# Specialization :msc
-# Experience. :2
-# Room No. :30
-1.Insert Docter Info 
-2. Fill Nurse Info
-3.Reception
-4.Exit
-Enter your choice :3
-Enter your Choice :
-1.Docter 
-2.Nurse1
-Enter Docter Name and ID :ankush 20
-1.Insert Docter Info 
-2. Fill Nurse Info
-3.Reception
-4.Exit
-Enter your choice :3
-Enter your Choice :
-1.Docter 
-2.Nurse1
-Enter Docter Name and ID :ankush
-20
-1.Insert Docter Info 
-2. Fill Nurse Info
-3.Reception
-4.Exit
-Enter your choice :
+#include<iostream>
+using namespace std;
+class person
+{
+public:
+	string name,salary,mobile_no;
+	int age,id;
+ void accept()
+ {
+   
+ cout<<"# Name :";
+ cin>>name;                                      
+ cout<<"# Age:";
+ cin>>age;
+ cout<<"# Mobile No. :";
+ cin>>mobile_no;
+ cout<<"# ID :";
+ cin>>id;
+ cout<<"# Salary :";
+ cin>>salary;
+ }
+};
+class docter:virtual public person
+{
+public:
+	string speci,exper,room_no;
+    void accept()
+	{
+		cout<<"# Specialization :";
+		cin>>speci;
+		cout<<"# Experience. :";
+		cin>>exper;
+	    cout<<"# Room No. :";
+	    cin>>room_no;
+	}
+};
+class nurse:virtual public person
+{
+public:
+	string shift,dept,ward;
+	void accept()
+	{
+		cout<<"# shift :";
+		cin>>shift;
+		cout<<"# Department :";
+		cin>>dept;
+		cout<<"# Ward :";
+		cin>>ward;
+	}
+};
+class reception:public nurse,public docter
+{
+public:
+    int choice;
+    string tempname;
+    int tempid;
+    void fun()
+    {
+	cout<<"Enter your Choice :\n1.Docter \n2.Nurse";
+	cin>>choice;
+	switch(choice)
+	{
+	    case 1: cout<<"Enter Docter Name and ID :";
+	    cin>>tempname>>tempid;
+	    if(tempname==name&&tempid==id)
+	    {
+	        cout<<"Docter Is Available !!!";
+	        cout<<"\nName\tSpecialization\tRoom no.\n";
+	        cout<<name<<"\t"<<speci<<"\t"<<room_no;
+	    }
+	    break;
+	    
+	    case 2:
+	    cout<<"Enter Nurse Name & ID :";
+	    cin>>tempname>>tempid;
+	    if(tempname==name&&tempid==id)
+	    {
+	        cout<<"Nurse is Available ";
+	        cout<<"\nName\tDepartment\tWard\n";
+	        cout<<name<<"\t"<<dept<<"\t"<<ward;
+	    }
+	    break;
+	    
+	    default:cout<<"Invalid Choice :";
+	}
+    }
+};
+int main()
+{
+   int ch;
+   reception obj;
+   do{
+   cout<<"1.Insert Docter Info \n2. Fill Nurse Info\n3.Reception\n4.Exit\nEnter your choice :";
+   cin>>ch;
+   switch(ch)
+   {
+       case 1:
+       int size;
+       cout<<"Enter No of Docter's info is to be Fill :";
+       cin>>size;
+       for(int i=0;i<size;i++)
+       {
+        cout<<"\nDocter_"<<i+1;
+        obj.person::accept();
+        obj.docter::accept();
+       }
+       break;
+       
+        case 2:
+       int size1;
+       cout<<"Enter No of Nurse's info is to be Fill :";
+       cin>>size1;
+       for(int i=0;i<size1;i++)
+       {
+        obj.person::accept();
+        obj.nurse::accept();
+       }
+       break;
+       
+       case 3:
+       obj.fun();
+       break;
+       
+       case 4: cout<<"You Are Exiting.....\n";
+       exit(0);
+       break;
+       
+       default:cout<<"Invalid Choice \n";
+   }
+   }while(1); 
+}
